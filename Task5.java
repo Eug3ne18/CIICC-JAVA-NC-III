71934d885093fd825811c786b695243579b42874
Eug3ne18 <EugPan3s@gmail.com>
2026-06-20 02:04:08 +0000

    5

---
+++ Task5.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Task5 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+        // read three numbers
+
+        System.out.print("Enter first number: ");
+
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+
+        int num2 = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+
+        int num3 = sc.nextInt();
+
+
+
+        // check equality
+
+        if (num1 == num2 && num2 == num3) {
+
+            System.out.println("All numbers are equal");
+
+        } else {
+
+            // find largest
+
+            int largest = num1;
+
+            if (num2 > largest) {
+
+                largest = num2;
+
+            }
+
+            if (num3 > largest) {
+
+                largest = num3;
+
+            }
+
+            System.out.println("The largest number is: " + largest);
+
+        }
+
+
+
+        
+
+    }
+
+}
